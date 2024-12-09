838f3a725fe3a5460a8fae910ec3275c4b273e73
Yevhen-Oleksandr Sytailo <sytyevhen@gmail.com>
2024-12-09 09:37:54 +0100

    small fix

--- Frequency-meter-based-on-STM32/Core/Src/main.c
+++ Frequency-meter-based-on-STM32/Core/Src/main.c
@@ -232,7 +232,7 @@ static void MX_TIM1_Init(void)
   htim1.Init.Period = 65535;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_IC_Init(&htim1) != HAL_OK)
   {
     Error_Handler();
